5af27e3f4fc1d59777822946fa24759b07672b2c
SovietBeast <pawlik1998@gmail.com>
2022-10-08 18:49:36 +0200

    2.1 2.2 2.3 2.4 should be done

---
+++ angular/app/src/app/chat-message-list/chat-message-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../message';
+
+@Component({
+  selector: 'app-chat-message-list',
+  templateUrl: './chat-message-list.component.html',
+  styleUrls: ['./chat-message-list.component.scss']
+})
+export class ChatMessageListComponent implements OnInit {
+
+  // Dane wejściowe komponentu - wiadomości oraz id użytkownika
+  @Input() messages:Message[] = [];
+  @Input() myId:number = 0;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular/app/src/app/chat/chat.component.ts
+++ angular/app/src/app/chat/chat.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpService } from '../http.service';
+import { User } from '../user';
+import { Message } from '../message';
 
 @Component({
   selector: 'app-chat',
@@ -8,6 +10,16 @@ import { HttpService } from '../http.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+
+ 
+  // lista użytkowników
+  users: User[] = [];
+
+  // lista wiadomości z wybranym uzytkownikiem
+  messagesToUser: Message[] = [];
+
+  // wybrany uzytkownik
+  selectedUser: User = {} as User;
 
   constructor(
     private router: Router,
@@ -17,6 +29,67 @@ export class ChatComponent {
     if (!httpService.isLogin) {
       this.router.navigate(['/login']);
     }
+  }
+
+  ngOnInit() {
+    this.reloadUsers();
+  }
+
+
+  // Funkcja zwracająca nasze ID
+  getMyId() {
+    return this.httpService.loginUserData.user_id;
+  }
+
+  // Funkcja wysyłająca wiadomość
+  sendMessage(e: string) {
+    this.httpService.sendMessages(new Message()).subscribe({
+      next: (data) => {
+        console.log("ChatComponent, onSubmit:", data);
+      },
+      error: (error) => {
+      }
+    });
+  }
+
+  // Funkcja przeładowująca listę użytkowników
+  reloadUsers() {
+    this.httpService.getUsers().subscribe({
+      next: (data) => {
+        if (data.hasOwnProperty("data")) {
+          let responseData = data as DataApiResponse<User>;
+          if (Array.isArray(responseData.data)) {
+            this.users = responseData.data;
+          }
+        }
+      },
+      error: (error) => {
+      }
+    });
+  }
+
+  // funkcja wywoływana gdy zostanie wybrany użytkownik na liście użytkowników
+  userSelected(user: User) {
+    this.selectedUser = user;
+    console.log("Selected user", this.selectedUser)
+    this.getMessagesWithSelectedUser();
+  }
+
+  // Funkcja pobierające listę wiadomości z danym użytkownikiem
+  getMessagesWithSelectedUser() {
+    // uzupełnij funkcję na podstawie funkcji realoadUsers
+    this.httpService.getMessages(this.selectedUser.user_id).subscribe({
+      next: (data) => {
+        if (data.hasOwnProperty("data")) {
+          let responseData = data as DataApiResponse<Message>;
+          if (Array.isArray(responseData.data)) {
+            this.messagesToUser = responseData.data;
+          }
+        }
+      },
+      error: (error) => {
+      }
+    });
   }
 
 }
